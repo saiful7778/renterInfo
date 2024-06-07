09cd43c7eeff0705095b088e2e8e1a07474741be
Saiful Islam <saiful.islam.rafi.88@gmail.com>
2024-06-08 03:06:17 +0600

    add use callback

--- client/src/hooks/useQueryParams.ts
+++ client/src/hooks/useQueryParams.ts
@@ -1,8 +1,12 @@
 import { useNavigate } from "@tanstack/react-router";
+import { useCallback } from "react";
 
 export default function useQueryParams() {
   const navigate = useNavigate({ from: "/" });
-  return (key: string, value: string | number) => {
-    navigate({ search: (prev) => ({ ...prev, [key]: value }) });
-  };
+  return useCallback(
+    (key: string, value: string | number) => {
+      navigate({ search: (prev) => ({ ...prev, [key]: value }) });
+    },
+    [navigate]
+  );
 }
