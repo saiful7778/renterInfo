e32327371dfbde41e0a6067f124ac421bbd3e771
Saiful Islam <saiful.islam.rafi.88@gmail.com>
2024-06-07 07:05:18 +0600

    add tanstack router main point

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,27 @@
+import { RouterProvider, createRouter } from "@tanstack/react-router";
 import { FC } from "react";
+import { routeTree } from "@/routeTree.gen";
+import ErrorPage from "@/pages/Error";
+import NotFound from "@/pages/NotFound";
+import { LoadingFullPage } from "./components/Loading";
+
+const router = createRouter({
+  routeTree,
+  defaultPendingComponent: LoadingFullPage,
+  defaultErrorComponent: ErrorPage,
+  defaultNotFoundComponent: NotFound,
+  defaultPreload: "intent",
+});
+
+// Register the router instance for type safety
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router;
+  }
+}
 
 const App: FC = () => {
-  return <div>App</div>;
+  return <RouterProvider router={router} />;
 };
 
 export default App;
