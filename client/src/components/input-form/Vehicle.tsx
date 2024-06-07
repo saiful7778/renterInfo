3da8c12f27ba12e56f21fd166e33a7f68c47168c
Saiful Islam <saiful.islam.rafi.88@gmail.com>
2024-06-08 03:06:06 +0600

    add error show and props

--- client/src/components/input-form/Vehicle.tsx
+++ client/src/components/input-form/Vehicle.tsx
@@ -4,45 +4,61 @@ import CommandInput from "@/components/CommandInput";
 import { InputCardProps } from "@/routes";
 import Form from "../ui/form";
 
-const Vehicle: FC<InputCardProps> = ({ control, loading }) => {
-  const frameworks = ["Next.js", "SvelteKit", "Nuxt.js", "Remix", "Astro"];
+interface VehicleProps extends InputCardProps {
+  isError?: string | null;
+  allVehicleType: string[];
+  allVehicle: string[];
+}
 
+const Vehicle: FC<VehicleProps> = ({
+  control,
+  loading,
+  isError,
+  allVehicleType,
+  allVehicle,
+}) => {
   return (
     <InputCard title="Vehicle Information">
-      <Form.field
-        control={control}
-        name="vehicleType"
-        render={({ field }) => (
-          <CommandInput
-            label="Vehicle Type"
-            placeholder="Select vehicle type"
-            searchPlaceholder="Search vehicle type"
-            data={frameworks}
+      {isError ? (
+        <div className="text-center text-destructive">Something went wrong</div>
+      ) : (
+        <>
+          <Form.field
+            control={control}
             name="vehicleType"
-            onChange={field.onChange}
-            value={field.value}
-            loading={loading}
-            required
+            render={({ field }) => (
+              <CommandInput
+                label="Vehicle Type"
+                placeholder="Select vehicle type"
+                searchPlaceholder="Search vehicle type"
+                data={allVehicleType}
+                name="vehicleType"
+                onChange={field.onChange}
+                value={field.value}
+                loading={loading}
+                required
+              />
+            )}
           />
-        )}
-      />
-      <Form.field
-        control={control}
-        name="vehicle"
-        render={({ field }) => (
-          <CommandInput
-            label="Vehicle"
-            placeholder="Select vehicle"
-            searchPlaceholder="Search vehicle"
-            data={frameworks}
+          <Form.field
+            control={control}
             name="vehicle"
-            onChange={field.onChange}
-            value={field.value}
-            loading={loading}
-            required
+            render={({ field }) => (
+              <CommandInput
+                label="Vehicle"
+                placeholder="Select vehicle"
+                searchPlaceholder="Search vehicle"
+                data={allVehicle}
+                name="vehicle"
+                onChange={field.onChange}
+                value={field.value}
+                loading={loading}
+                required
+              />
+            )}
           />
-        )}
-      />
+        </>
+      )}
     </InputCard>
   );
 };
