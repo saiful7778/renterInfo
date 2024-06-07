5b9260b8be9002c6fa32718337f1ff52ccc9b6ef
Saiful Islam <saiful.islam.rafi.88@gmail.com>
2024-06-08 03:05:30 +0600

    add additional section

---
+++ client/src/components/input-form/Additional.tsx
@@ -0,0 +1,20 @@
+import { FC } from "react";
+import InputCard from "@/components/InputCard";
+
+interface AdditionalProps {
+  features?: string[];
+}
+
+const Additional: FC<AdditionalProps> = ({ features }) => {
+  return (
+    <InputCard title="Additional Charges" showAccrodion>
+      {features?.map((ele, idx) => (
+        <div className="p-2" key={`features-${idx}`}>
+          {ele}
+        </div>
+      ))}
+    </InputCard>
+  );
+};
+
+export default Additional;
