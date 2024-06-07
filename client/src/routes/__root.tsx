59af944cfded1ec97eee416c2a425cd940221fe2
Saiful Islam <saiful.islam.rafi.88@gmail.com>
2024-06-07 22:43:04 +0600

    update style

--- client/src/routes/__root.tsx
+++ client/src/routes/__root.tsx
@@ -3,7 +3,7 @@ import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 
 export const Route = createRootRoute({
   component: () => (
-    <div className="relative min-h-screen w-full overflow-x-hidden font-poppins">
+    <div className="relative min-h-screen w-full px-2 py-2 md:py-8 overflow-x-auto font-poppins">
       <main className="container">
         <Outlet />
       </main>
