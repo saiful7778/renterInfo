fe14a2c0232aac1bfcfa213c3f48f08650691345
Saiful Islam <saiful.islam.rafi.88@gmail.com>
2024-06-07 07:11:53 +0600

    update and fix error

--- client/src/pages/NotFound.tsx
+++ client/src/pages/NotFound.tsx
@@ -1,3 +1,4 @@
+import Button from "@/components/ui/button";
 import { Link } from "@tanstack/react-router";
 import { FC } from "react";
 
@@ -6,9 +7,9 @@ const NotFound: FC = () => {
     <div className="flex min-h-screen w-full items-center justify-center">
       <div className="space-y-3 text-center">
         <h3 className="text-3xl font-bold">404! not found</h3>
-        <Link to="/" className="capitalize italic text-blue-600 underline">
-          go to home
-        </Link>
+        <Button asChild>
+          <Link to="/">go to home</Link>
+        </Button>
       </div>
     </div>
   );
