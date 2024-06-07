12d6d7ac91d9895e616592c707b5dcdb338f23da
Saiful Islam <saiful.islam.rafi.88@gmail.com>
2024-06-08 03:06:55 +0600

    add selected user data manage

--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -10,9 +10,10 @@ import { type Control, useForm } from "react-hook-form";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { reservationSchema } from "@/lib/schemas/reservation";
-import { useEffect, useState } from "react";
+import { useEffect, useLayoutEffect, useState } from "react";
 import Accordion from "@/components/ui/accordion";
 import useQueryParams from "@/hooks/useQueryParams";
+import getTimeDuration from "@/lib/getTimeDuration";
 
 export const Route = createFileRoute("/")({
   validateSearch: (search: Record<string, unknown>) => ({
@@ -20,6 +21,7 @@ export const Route = createFileRoute("/")({
     pickupDate: search.pickupDate as string,
     returnDate: search.returnDate as string,
     duration: search.duration as string,
+    durationDisplay: search.durationDisplay as string,
     discount: search.discount as string,
     vehicleType: search.vehicleType as string,
     vehicle: search.vehicle as string,
@@ -36,12 +38,30 @@ export interface InputCardProps {
   loading: boolean;
 }
 
+type allDataTypes = {
+  id: string;
+  make: string;
+  model: string;
+  year: number;
+  type: string;
+  seats: number;
+  bags: number;
+  features: string[];
+  rates: {
+    hourly: number;
+    daily: number;
+    weekly: number;
+  };
+  imageURL: string;
+};
+
 function Index() {
   const {
     reservationId,
     pickupDate,
     returnDate,
     duration,
+    durationDisplay,
     discount,
     vehicleType,
     vehicle,
@@ -51,6 +71,26 @@ function Index() {
     phone,
   } = Route.useSearch();
   const [loading, setLoading] = useState<boolean>(false);
+  const [isError, setIsError] = useState<string | null>(null);
+  const [allVehicleType, setAllVehicleType] = useState<string[]>([]);
+  const [allVehicleData, setAllVehicleData] = useState<allDataTypes[]>([]);
+  const [selectedVehicle, setSelectedVehicle] = useState<allDataTypes>({
+    id: "",
+    make: "",
+    model: "",
+    year: 0,
+    type: "",
+    seats: 0,
+    bags: 0,
+    features: [],
+    rates: {
+      hourly: 0,
+      daily: 0,
+      weekly: 0,
+    },
+    imageURL: "",
+  });
+  const [total, setTotal] = useState(0);
   const queryParams = useQueryParams();
 
   const form = useForm<z.infer<typeof reservationSchema>>({
@@ -60,7 +100,7 @@ function Index() {
       pickupDate: pickupDate || "",
       returnDate: returnDate || "",
       duration: duration || "",
-      durationDisplay: duration || "",
+      durationDisplay: durationDisplay || "",
       discount: discount || "",
       vehicleType: vehicleType || "",
       vehicle: vehicle || "",
@@ -71,46 +111,100 @@ function Index() {
     },
   });
 
+  const vehicleTypeValue = form.watch("vehicleType");
+  const vehicleValue = form.watch("vehicle");
   const pickupDateValue = form.watch("pickupDate");
+  const durationDisplayValue = form.watch("durationDisplay");
   const returnDateValue = form.watch("returnDate");
 
-  useEffect(() => {
-    const timeout = setTimeout(() => {
-      const start = new Date(pickupDateValue);
-      const end = new Date(returnDateValue);
-
-      if (isNaN(start.getTime()) || isNaN(end.getTime())) return "";
-
-      const duration = end.getTime() - start.getTime();
-
-      const oneDay = 1000 * 60 * 60 * 24;
-
-      const weeks = Math.floor(duration / (oneDay * 7));
-      const days = Math.floor((duration % (oneDay * 7)) / oneDay);
-      const hours = Math.floor((duration % oneDay) / (1000 * 60 * 60));
-
-      let durationTime = "";
-
-      if (weeks > 0) {
-        durationTime += `${weeks}w ${days}d ${hours}h`;
-      } else if (days > 0) {
-        durationTime += `${days}d ${hours}h`;
-      } else {
-        durationTime += `${hours}h`;
+  useLayoutEffect(() => {
+    (async () => {
+      try {
+        setIsError(null);
+        const res = await fetch(
+          "https://exam-server-7c41747804bf.herokuapp.com/carsList"
+        );
+        const data: { status: string; data: allDataTypes[] } = await res.json();
+        if (data.status !== "success") {
+          throw new Error();
+        }
+        setAllVehicleData([]);
+        setAllVehicleType([]);
+        if (data && data?.data?.length > 0) {
+          setAllVehicleData(data.data);
+          for (const x of data.data) {
+            setAllVehicleType((props) => [...new Set([...props, x.type])]);
+          }
+        }
+      } catch (err) {
+        if (err instanceof Error) {
+          setIsError(err.message);
+        }
       }
-      queryParams("duration", duration);
+    })();
+  }, [vehicleValue]);
 
-      form.setValue("duration", String(duration));
-      form.setValue("durationDisplay", String(durationTime));
-    }, 500);
+  useEffect(() => {
+    setSelectedVehicle((props) => ({
+      ...props,
+      ...allVehicleData.find(
+        (ele) =>
+          ele.make === vehicleValue.split(", ")[0] &&
+          ele.model === vehicleValue.split(", ")[1]
+      ),
+    }));
+  }, [vehicleValue, allVehicleData, durationDisplayValue]);
 
-    return () => {
-      clearTimeout(timeout);
-    };
-  }, [pickupDateValue, returnDateValue, form, queryParams]);
+  useEffect(() => {
+    const rates = selectedVehicle?.rates;
+    const time = durationDisplayValue.replace(/\D/g, "").split("").reverse();
 
-  const handleSubmit = async (data: z.infer<typeof reservationSchema>) => {
+    let totalPrice = 0;
+
+    if (time[0]) {
+      totalPrice += parseFloat(time[0]) * rates?.hourly;
+    }
+    if (time[1]) {
+      totalPrice += parseFloat(time[1]) * rates?.daily;
+    }
+    if (time[2]) {
+      totalPrice += parseFloat(time[2]) * rates?.weekly;
+    }
+    setTotal(totalPrice);
+  }, [selectedVehicle, durationDisplayValue]);
+
+  useEffect(() => {
+    const { duration, durationDisplay } = getTimeDuration(
+      pickupDateValue,
+      returnDateValue
+    );
+    queryParams("duration", duration);
+    queryParams("durationDisplay", durationDisplay);
+
+    form.setValue("duration", String(duration));
+    form.setValue("durationDisplay", String(durationDisplay));
+    return () => {};
+  }, [pickupDateValue, returnDateValue, queryParams, form]);
+
+  const handleSubmit = async (e: z.infer<typeof reservationSchema>) => {
     setLoading(true);
+    const data = {
+      reservationId: e.reservationId,
+      pickupDate: e.pickupDate,
+      returnDate: e.returnDate,
+      duration: parseFloat(e.duration),
+      discount: parseFloat(e.discount || "0"),
+      vehicleType: e.vehicleType,
+      vehicle: {
+        make: selectedVehicle.make,
+        model: selectedVehicle.model,
+      },
+      firstName: e.firstName,
+      lastName: e.lastName,
+      email: e.email,
+      phone: e.phone,
+      total,
+    };
     console.log(data);
     setLoading(false);
   };
@@ -126,13 +220,29 @@ function Index() {
           <div className="flex gap-4 flex-wrap">
             <div className="w-full max-w-sm">
               <Reservation control={form.control} loading={loading} />
-              <Vehicle control={form.control} loading={loading} />
+              <Vehicle
+                control={form.control}
+                loading={loading}
+                isError={isError}
+                allVehicleType={allVehicleType}
+                allVehicle={allVehicleData
+                  .filter((ele) => ele.type === vehicleTypeValue && ele.type)
+                  .map((ele) => `${ele.make}, ${ele.model}`)}
+              />
             </div>
             <div className="w-full max-w-sm">
               <Customer control={form.control} loading={loading} />
-              <Additional />
+              <Additional features={selectedVehicle.features} />
             </div>
-            <Charges />
+            <div className="w-full max-w-sm">
+              <Charges rates={selectedVehicle.rates} total={total} />
+              {selectedVehicle && (
+                <img
+                  src={selectedVehicle.imageURL}
+                  alt={`${selectedVehicle.make} ${selectedVehicle.model} image`}
+                />
+              )}
+            </div>
           </div>
         </Accordion>
       </form>
